03153a770d673df9d0a848b73d4cd43855134e1d
GravyHTX <godisgravy@gmail.com>
2020-03-27 22:39:12 -0500

    heroku1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/finance", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/finance", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
